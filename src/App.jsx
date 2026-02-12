c0e412636d2f526fd13938eddea738e68a8736c2
Daniel <d0583258844@gmail.com>
2026-02-12 12:41:53 +0200

    a

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import "./App.css";
 import { useState } from "react";
 import { terorist } from "./data/terrorists_data";
 import Rows from "./components/all-rows/Rows";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   const [teror, setTerorist] = useState(terorist);
+  const [filter, setFilter] = useState({});
+
   return (
     <>
+      <Navbar setFilter={setFilter} />
       <Rows teror={teror} />;
     </>
   );
